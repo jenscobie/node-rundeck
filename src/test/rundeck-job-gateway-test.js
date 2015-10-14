3cc0b793d5458a1dec0e6ceea4cec560bc93147e
jenscobie <jenscobie@gmail.com>
2015-10-13 23:03:49 -0700

    Using nodeunit to validate error on callback

--- src/test/rundeck-job-gateway-test.js
+++ src/test/rundeck-job-gateway-test.js
@@ -1,12 +1,49 @@
 var assert = require('assert');
 var fs = require('fs');
 var request = require('request');
+var nodeunit = require('nodeunit');
 var sinon = require('sinon');
 var util = require('util');
 var execute = require('../lib/rundeck-job-gateway');
 
 describe('Rundeck Gateway', function () {
-  describe('Execute Job', function () {
+  describe('Rundeck API is available', function () {
+
+    var token = 'token';
+    var id = 1;
+    var payload = fs.readFileSync('./src/test/data/job-run.xml', 'ascii');
+
+    before(function(done) {
+      sinon
+        .stub(request, 'get')
+        .withArgs({
+          url: util.format("http://example.com:4000/api/13/job/%s/run", id),
+          headers: {
+            'User-Agent': 'node-rundeck',
+            'X-Rundeck-Auth-Token': token
+          }
+        }, sinon.match.any)
+        .yields(null,
+          { statusCode: 200 },
+          payload);
+      done();
+    });
+
+    after(function(done) {
+      request.get.restore();
+      done();
+    });
+
+    it('should return payload on job execution', function(done) {
+      execute('http://example.com', 4000, 13, token, id, function (err, result) {
+        assert(request.get.called);
+        assert.equal(result, payload)
+        done();
+      });
+    });
+  });
+
+  describe('Rundeck API is down', function () {
 
     var token = 'token';
     var id = 1;
@@ -21,13 +58,20 @@ describe('Rundeck Gateway', function () {
             'X-Rundeck-Auth-Token': token
           }
         }, sinon.match.any)
-        .yields(null, fs.readFileSync('./src/test/data/job-run.xml', 'ascii'));
+        .yields(null,
+          { statusCode: 200 },
+          fs.readFileSync('./src/test/data/job-run.xml', 'ascii'));
       done();
     });
 
-    it('should GET /job/id/run resource', function(done) {
+    after(function(done) {
+      request.get.restore();
+      done();
+    });
+
+    it('should return error on job execution failure', function(done) {
       execute('http://example.com', 4000, 13, token, id, function (err, result) {
-        assert(request.get.called);
+        assert.ifError(err);
         done();
       });
     });
